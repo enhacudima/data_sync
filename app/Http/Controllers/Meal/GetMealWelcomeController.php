a193b429f9934977db8e546edb8c39b7fca8280c
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-05-18 16:46:09 +0200

    first verson

--- app/Http/Controllers/Meal/GetMealWelcomeController.php
+++ app/Http/Controllers/Meal/GetMealWelcomeController.php
@@ -20,7 +20,7 @@ class GetMealWelcomeController extends Controller
 
     public function getPagmMals()
     {
-    	$data=Meals::with('mealCuisine','mealCategory','mealUser.userType','mealAllergies.allergiesSync','mealAllergies.allergiesIngredients','mealtiming','mealPrices','mealPrices.priceCurrency','mealType','mealFiles','mealFile','mealChefs','mealTags.tagName')->orderby('end_date','asc')->where('start_date', '<=', Now())->where('end_date', '>=', Now())->paginate(20);
+    	$data=Meals::with('mealCategory','mealTags.tagName','mealOptions')->orderby('end_date','asc')->where('start_date', '<=', Now())->where('end_date', '>=', Now())->paginate(20);
 
         return response()->json($data, 200);
     }
