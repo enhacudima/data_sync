ace29a1e05818c9933a2200eded5c44df2a77db8
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-05-20 19:47:07 +0200

    verson 1.1.0

--- config/database.php
+++ config/database.php
@@ -60,9 +60,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::ATTR_EMULATE_PREPARES => true,
-
             ]) : [],
+            PDO::ATTR_EMULATE_PREPARES => true, PDO::MYSQL_ATTR_COMPRESS => true,
         ],
 
         'pgsql' => [
