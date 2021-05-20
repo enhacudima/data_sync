c8144c15bb64d42f3ee4d7e037bc2fa7073d6902
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-05-20 20:09:01 +0200

    verson 1.1.0

--- app/UserV.php
+++ app/UserV.php
@@ -8,6 +8,7 @@ class UserV extends Model
 {
 
     protected $table = 'users_v';
+    protected $connection = 'mysqlView';//via connectio 2
 
     protected $fillable = [
         'name', 'email','lastName','dataBrith','phone1','prefix_phone_1','userName','type','status','prefix_id', 'key','mode','location_id','fullAddr','country',
