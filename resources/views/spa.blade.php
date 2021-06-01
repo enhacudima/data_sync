8634ba96086cb275228fc96ca9eadee2374ac626
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-06-01 14:42:56 +0200

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" value="{{ csrf_token() }}"/>
         <link rel="icon" type="image/jpg" href="{{URL::asset('/storage/icons/icon.png')}}"/>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
         <title>Concurso Público</title>
     </head>
     <body>
