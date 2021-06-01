6b7b7aba0780f53875a2c536ef7dce4a1900069d
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-06-01 14:54:21 +0200

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -11,9 +11,6 @@
         <title>Concurso Público</title>
     </head>
     <body>
-        <div id="app">
-            <app></app>
-        </div>
-        <script src="{{ mix('/js/app.js') }}"></script>
+
     </body>
 </html>
