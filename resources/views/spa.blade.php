77f43b3ef94c2b93a63b11913b9e45aa8a24fa9e
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-06-01 14:49:38 +0200

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -1,19 +1,19 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" value="{{ csrf_token() }}" />
-    <link rel="icon" type="image/jpg" href="{{URL::asset('/storage/icons/icon.png')}}"/>
-    <title>Concurso Público</title>
-
-    <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
-</head>
-
-<body>
-    <div id="app"></div>
-    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
-</body>
-
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" value="{{ csrf_token() }}"/>
+        <link rel="icon" type="image/jpg" href="{{URL::asset('/storage/icons/icon.png')}}"/>
+        <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+        <title>Concurso Público</title>
+    </head>
+    <body>
+        <div id="app">
+            <app></app>
+        </div>
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
 </html>
