f076e8c53db395811443810e94b091a4a5c3bef8
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-07-01 11:58:29 +0200

    fix

--- app/Http/Controllers/Meal/GetMealWelcomeController.php
+++ app/Http/Controllers/Meal/GetMealWelcomeController.php
@@ -37,7 +37,7 @@ class GetMealWelcomeController extends Controller
                 ->orwhere('start_date','like',"%".$search."%")
                 ->orwhere('end_date','like',"%".$search."%")
                 ->orwhereHas('mealUser', function ($q) use ($search) {
-                    $q->orwhere('name', 'like', '%' .$search. '%')
+                    $q->orwhere('nam', 'like', '%' .$search. '%')
                     ->orwhere('lastName', 'like', '%' .$search. '%');
                 })
                 ->orwhere('reference','like',"%".$search."%");
