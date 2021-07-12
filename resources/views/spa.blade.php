1f008347c5af442605853b7176ed6101399c784f
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-07-12 18:26:16 +0200

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
         <?php
             header('Access-Control-Allow-Origin: *');
             header("Access-Control-Allow-Credentials: true");
@@ -8,6 +7,7 @@
             header('Access-Control-Max-Age: 1000');
             header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
         ?>
+    <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" value="{{ csrf_token() }}"/>
