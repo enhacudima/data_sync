89a6da31283c294999baaaba2bdd4debc07d37fc
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-07-12 18:49:10 +0200

    fix

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -3,7 +3,7 @@
     <?php
         // Allow from any origin
         if (isset($_SERVER['HTTP_ORIGIN'])) {
-            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header("Access-Control-Allow-Origin: * ");
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Max-Age: 86400');    // cache for 1 day
         }
